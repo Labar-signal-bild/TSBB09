e65456721f7ad0f8db35dc50a1f87aab271ddb93
Alexander Poole <alepo020@student.liu.se>
2015-11-09 13:21:53 +0100

    Added correct functions

--- LabA/LabA.m
+++ LabA/LabA.m
@@ -1,5 +1,7 @@
 %% LabA 
 
+%% Setup
+
 addpath /site/edu/bb/Bildsensorer/A-DigitalCamera/
 addpath /site/edu/bb/Bildsensorer/A-DigitalCamera/shadingcorr
 addpath /site/edu/bb/Bildsensorer/A-DigitalCamera/bayer
@@ -7,4 +9,30 @@ addpath /site/edu/bb/Bildsensorer/A-DigitalCamera/bayer
 shadcorr 
 
 close all
+
+%% 2.1 
+
+%Question
+% Gives a better linnear approximation in the spectrum which is the camera
+% is more likely to be used in.
+
+bA = darkimage;
+bB = whiteimage;
+b  = origimage;
+
+fA = mean(mean(bA)); % 118,3710
+fB = mean(mean(bB)); % 206,6197
+
+c  =1./(bB-bA);
+d  = (fA.*bB-fB.*bA)./(fB-fA);
+
+f = c.*(b+d);
+
+figure(1)
+imagesc(f); axis image; axis off; colormap(jet);
+
+
+%Question 
+%no
+
 
