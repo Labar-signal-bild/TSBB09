f8d2fde5bf67e9e1eda4d6eb5b33393da90497fa
Alexander Poole <alepo020@student.liu.se>
2015-11-09 11:05:04 +0100

    Added files

---
+++ LabA/LabA.m
@@ -0,0 +1,10 @@
+%% LabA 
+
+addpath /site/edu/bb/Bildsensorer/A-DigitalCamera/
+addpath /site/edu/bb/Bildsensorer/A-DigitalCamera/shadingcorr
+addpath /site/edu/bb/Bildsensorer/A-DigitalCamera/bayer
+
+shadcorr 
+
+close all
+
